9f5b20a908a648df9f1797bc280a70426b95dfcc
tikonexdev <pastolektorreapload@gmail.com>
2022-04-03 15:20:20 +0200

    podstawy i endl

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+float x,y;
+long suma;
+int main()
+{
+    cout << "podaj pierwsza liczba" << endl;
+    cin >>x;
+cout << " podaj druga liczba "<<endl;
+cin >>y;
+cout <<"menu glowne" <<endl ;
+cout << "************************" <<endl;
+cout << "1. dodawanie" <<endl;
+cout <<"2. odejmowanie" <<endl;
+
+}
