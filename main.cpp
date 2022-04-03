f51defdd4c4815bd2cb56ee39c97778fa9383664
tikonexdev <pastolektorreapload@gmail.com>
2022-04-03 15:50:54 +0200

    s

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
 using namespace std;
-float x,y;
-long suma;
+int x;
+int y;
+long long num = 77778;
+long long sqr = num * num;
+int wybor;
 int main()
-{
+{while(true){
+
     cout << "podaj pierwsza liczba" << endl;
     cin >>x;
 cout << " podaj druga liczba "<<endl;
@@ -15,6 +19,36 @@ cout << "1. dodawanie" <<endl;
 cout <<"2. odejmowanie" <<endl;
 cout << "3. mnozenie" <<endl;
 cout <<"4. dzielenie" <<endl;
+cout <<"5. wyjdz" <<endl;
+cout << "************************" <<endl;
+cout << "wybierz opcje " << endl;
+cin >> wybor;
+cout <<endl;
+switch (wybor){
+    case 1:{
+        cout <<"suma= " << x + y;
+    }
+    break;
+case 2:{
+cout <<"suma = " << x-y;
+}
+break;
+case 3:{
+cout  << sqr ;
+}
+case 4:{
+    cout << x/y;
+break;
+case 5:{
+    exit(0);
+}
+default :
+    {
 
-
+}
+break;
+}
+}
+getchar();
+}
 }
